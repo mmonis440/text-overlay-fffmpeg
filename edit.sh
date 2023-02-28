598e1bf4577b95d8b47faeaba3f3f52a4131b3d5
mmonis440 <mmonis440@gmail.com>
2023-02-28 15:41:06 +0530

    .sh file updated

---
+++ edit.sh
@@ -0,0 +1,3 @@
+# command for adding text overlay to video 
+# first install ffmpeg package via their website ....
+ffmpeg -i input_video.mp4 -vf "drawtext=fontfile=/path/to/font.ttf:text='HI INTERACTLY.VIDEO TEAM':fontcolor=White:fontsize=35:box=1:boxcolor=Maroon:bordercolor=Aquamarine:boxborderw=30:x=(w-text_w)/2:y=(h-text_h)/2" -codec:a copy interactive2.mp4
